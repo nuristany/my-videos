9b9a85e661884d9e0dcbcabe1906a79ca1f49feb
Qais <nuristanyqais@gmail.com>
2024-01-18 12:27:15 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as fakeMovieService from './fakeMovieService';
+import  { useState } from 'react';
 
 function App() {
+  
+  const [movies, setMovies] = useState(fakeMovieService.getMovies());
+  
+  const handleDelete = (movie) =>{
+    const newMovie = movies.filter(m => m._id !== movie._id)
+    setMovies(newMovie)
+  }
+  const movieList = movies.map(movie => (
+    <tr key={movie._id}>
+      <td>{movie.title}</td>
+      <td>{movie.genre.name}</td>
+      <td>{movie.numberInStock}</td>
+      <td>{movie.dailyRentalRate}</td>
+      <td>
+        <button onClick={() => handleDelete(movie)} className='btn btn-danger'>Delete</button>
+      </td>
+      <td>
+        <img src={movie.image} className='movie-image' alt='movies' />
+      </td>
+    </tr>
+
+  ))
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='main'>
+
+    {movies.length === 0 ?<p>No movies available.</p> : <p>Showing {movies.length} movies in the database</p>}
+    
+
+    <div className="Table mt-5">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {movieList}
+        </tbody>
+          
+      </table>
+    </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+    
+    
---
+++ src/fakeMovieService.js
@@ -0,0 +1,116 @@
+import * as genresAPI from "./fakeGenreService";
+import terminator from './images/the-terminator.jpg'
+import die from './images/die hard.jpeg'
+import getout from './images/getout.jpeg'
+import trip from './images/trip to italy.jpeg'
+import avengers from './images/The Avengers.jpeg'
+import sixth from './images/The Sixth Sense.jpeg'
+import gone from './images/gone girl.jpeg'
+import wedding from './images/Wedding Crashers.jpeg'
+import airplane from './images/airplane.jpeg'
+
+
+const movies = [
+  {
+    _id: "5b21ca3eeb7f6fbccd471815",
+    title: "Terminator",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 6,
+    dailyRentalRate: 2.5,
+    publishDate: "2018-01-03T19:04:28.809Z",
+    image:terminator
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471816",
+    title: "Die Hard",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 5,
+    dailyRentalRate: 2.5,
+    image:die
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471817",
+    title: "Get Out",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 8,
+    dailyRentalRate: 3.5,
+    image:getout
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471819",
+    title: "Trip to Italy",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+    image:trip
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181a",
+    title: "Airplane",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+    image:airplane
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181b",
+    title: "Wedding Crashers",
+    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+    image:wedding
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181e",
+    title: "Gone Girl",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 7,
+    dailyRentalRate: 4.5,
+    image:gone
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd47181f",
+    title: "The Sixth Sense",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
+    numberInStock: 4,
+    dailyRentalRate: 3.5,
+    image:sixth
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471821",
+    title: "The Avengers",
+    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    numberInStock: 7,
+    dailyRentalRate: 3.5,
+    image:avengers
+  }
+];
+
+export function getMovies() {
+  return movies;
+}
+
+export function getMovie(id) {
+  return movies.find(m => m._id === id);
+}
+
+export function saveMovie(movie) {
+  let movieInDb = movies.find(m => m._id === movie._id) || {};
+  movieInDb.name = movie.name;
+  movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
+  movieInDb.numberInStock = movie.numberInStock;
+  movieInDb.dailyRentalRate = movie.dailyRentalRate;
+
+  if (!movieInDb._id) {
+    movieInDb._id = Date.now();
+    movies.push(movieInDb);
+  }
+
+  return movieInDb;
+}
+
+export function deleteMovie(id) {
+  let movieInDb = movies.find(m => m._id === id);
+  movies.splice(movies.indexOf(movieInDb), 1);
+  return movieInDb;
+}
